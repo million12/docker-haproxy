ab0b076fa61158e17f2110d725e6305178f76056
Mathias Hansen <me@codemonkey.io>
2019-11-04 20:19:34 -0500

    Do not make haproxy a daemon, so we can get log output via stdout

--- container-files/bootstrap.sh
+++ container-files/bootstrap.sh
@@ -12,7 +12,7 @@ HAPROXY_USER_PARAMS=$@
 
 # Internal params
 HAPROXY_PID_FILE="/var/run/haproxy.pid"
-HAPROXY_CMD="/usr/local/sbin/haproxy -f ${HAPROXY_CONFIG} ${HAPROXY_USER_PARAMS} -D -p ${HAPROXY_PID_FILE}"
+HAPROXY_CMD="/usr/local/sbin/haproxy -f ${HAPROXY_CONFIG} ${HAPROXY_USER_PARAMS} -p ${HAPROXY_PID_FILE}"
 HAPROXY_CHECK_CONFIG_CMD="/usr/local/sbin/haproxy -f ${HAPROXY_CONFIG} -c"
 
 # Iptable commands
@@ -58,7 +58,7 @@ grep --silent -e "web.server" /etc/hosts || echo "127.0.0.1 web.server" >> /etc/
 
 log $HAPROXY_CMD && print_config
 $HAPROXY_CHECK_CONFIG_CMD
-$HAPROXY_CMD
+$HAPROXY_CMD &
 # Exit immidiately in case of any errors or when we have interactive terminal
 if [[ $? != 0 ]] || test -t 0; then exit $?; fi
 log "HAProxy started with $HAPROXY_CONFIG config, pid $(cat $HAPROXY_PID_FILE)." && log
